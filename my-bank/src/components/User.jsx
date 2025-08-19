49c38d1d9bb2ca7adc5c7ec1625ae4a50d6992f1
Rodolphe <rodolphe.of@gmail.com>
2025-08-19 17:39:01 +0200

    avatar + font

--- my-bank/src/components/User.jsx
+++ my-bank/src/components/User.jsx
@@ -1,8 +1,9 @@
 const User = ({username}) => {
+    const urlImg = `http://localhost:5173/src/assets/avatars/avatar_${username}.webp`
     return (
         <div className="flexContainer">
             <span className="user">{username}</span>
-            <img src={`../assets/avatar_${username}.webp`} alt="" />
+            <img className="avatar" src={urlImg} alt="" />
         </div>
     )
 }
