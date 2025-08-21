9b76804a2f42a4810b9afd85ed9eb41be60ca68e
Rodolphe <rodolphe.of@gmail.com>
2025-08-21 16:12:18 +0200

    correction map

--- emojis-search-form/src/App.jsx
+++ emojis-search-form/src/App.jsx
@@ -7,6 +7,7 @@ import LineResult from './components/LineResult'
 function App() {
   const [search, setSearch] = useState("")
   console.log("search : "+search);
+
   return (
     <>
       <Header setSearch={setSearch} search={search} />
@@ -14,14 +15,12 @@ function App() {
         emojiList.map( (emoji, index) => {
           // création tableau des keywords strings de chaque emoji
           const emojiTab = emoji.keywords.split(" ");
-
-          console.log(emojiTab.includes(search));
          
-          return () => {
+          return (
             // check si le tableau contient la recherche pour afficher le component
             emojiTab.includes(search) &&
-            <LineResult key={index} search={search} />
-          }
+            <LineResult key={index} emoji={emoji} />
+          )
         })
       }
       
