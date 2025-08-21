ae60795121482b354be3cc819adb092d095cf13f
Rodolphe <rodolphe.of@gmail.com>
2025-08-21 17:29:43 +0200

    click&copy emoji

--- emojis-search-form/src/components/LineResult.jsx
+++ emojis-search-form/src/components/LineResult.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 const LineResult = ({emoji}) => {
+    const [active, setActive] = useState(false)
+    const handleClickCopy = () => {
+        navigator.clipboard.writeText(emoji.symbol);
+        setActive(true)
+    }
+    const handleMouseOut = () => {
+        setActive(false)
+    }
     return (
-        <div className="lineResult">{emoji.symbol} : {emoji.title}</div>
+        <div className={`lineResult ${active ? 'active' : ''}`} onClick={handleClickCopy} onMouseLeave={handleMouseOut}>{emoji.symbol} : {emoji.title}</div>
     )
 }
 export default LineResult
